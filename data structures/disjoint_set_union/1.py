5f6a2adf868e61fe0112753ed02bf3b9218a5ecc
Pitik Dmitry <pitik.dmitry@mail.ru>
2018-03-18 13:35:19 +0300

    dsu tasks done

--- data structures/disjoint_set_union/1.py
+++ data structures/disjoint_set_union/1.py
@@ -3,7 +3,7 @@ class DisjointSetUnion:
     def __init__(self):
         self._parents = []
         self._ranks = []
-        self._max_rank = 1
+        self._max_rank = 0
 
     def add(self, parent, table_size):
         self._parents.append(parent)
@@ -12,9 +12,9 @@ class DisjointSetUnion:
             self._max_rank = table_size
 
     def find(self, i):
-        while i != self._parents[i]:
-            i = self._parents[i]
-        return i
+        if i != self._parents[i]:
+            self._parents[i] = self.find(self._parents[i])
+        return self._parents[i]
 
     def union(self, dest, source) -> int:
         dest_root = self.find(dest)
@@ -23,8 +23,9 @@ class DisjointSetUnion:
         if dest_root == source_root:
             return self._max_rank
 
-        self._parents[source_root] = self._parents[dest_root]
         self._ranks[dest_root] = self._ranks[source_root] + self._ranks[dest_root]
+        self._ranks[source_root] = 0
+        self._parents[source_root] = self._parents[dest_root]
         if self._ranks[dest_root] > self._max_rank:
             self._max_rank = self._ranks[dest_root]
 
@@ -34,7 +35,6 @@ class DisjointSetUnion:
 n, m = map(int, input().split())
 dsu = DisjointSetUnion()
 table_sizes = list(map(int, input().split()))
-
 
 
 for i in range(0, len(table_sizes)):
