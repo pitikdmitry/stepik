9a5115476f00f98a0b58ecd48b215be4e854d6b9
Pitik Dmitry <pitik.dmitry@mail.ru>
2018-03-25 15:53:01 +0300

    not done

--- data structures/hashtables/3.py
+++ data structures/hashtables/3.py
@@ -1,36 +1,37 @@
 from random import randint
 
-p = 20
-x = 5
+p = 10123407
+x = 53
+
+
+def check_strings(text: str, pattern: str):
+    if len(text) != len(pattern):
+        return -1
+    for i in range(0, len(text)):
+        if text[i] != pattern[i]:
+            return -1
+
+    return 0
 
 
 def get_hash(text: str):
     hash = 0
-    p = 20
-    x = 5
     for i in range(0, len(text)):
-        hash += (ord(text[i]) * (x ** i)) % p
-        em3 = (ord(text[i]) * (x ** i)) % p
+        hash += (ord(text[i]) * (x ** i))
     return hash
 
 
 def get_second_hash(previous_hash: int, previous_str: str, current_str: str):
-    p = 20
-    x = 5
-    previous_str_last_symbol_hash = (ord(previous_str[len(previous_str) - 1]) * (x ** (len(previous_str) - 1))) % p
-    new_str_first_symbol_hash = (ord(current_str[0]) * (x ** 0)) % p
-    hash = new_str_first_symbol_hash + (previous_hash - previous_str_last_symbol_hash) * x
-    return hash
+    previous_str_last_symbol_hash = (ord(previous_str[len(previous_str) - 1]) * (x ** (len(previous_str) - 1)))
+    new_str_first_symbol_hash = (ord(current_str[0]) * (x ** 0))
+    return new_str_first_symbol_hash + ((previous_hash - previous_str_last_symbol_hash) * x)
 
 
 def rabin_karp_alg(pattern: str, text: str) -> None:
     if len(text) < len(pattern) or len(pattern) < 1:
         return
-    p = 20
-    x = 5
 
     P = len(pattern)
-    em2 = text[len(text) - P:]
     last_hash = get_hash(text[len(text) - P:])
     pattern_hash = get_hash(pattern)
     hash_arr = [last_hash]
@@ -38,13 +39,13 @@ def rabin_karp_alg(pattern: str, text: str) -> None:
     for i in range(len(text) - P - 1, -1, -1):
         first_index = i
         last_index = i + P
-        em = text[first_index:last_index]
         new_hash = get_second_hash(hash_arr[len(hash_arr) - 1], text[first_index + 1:last_index + 1], text[first_index:last_index])
         hash_arr.append(new_hash)
 
     for i in range(len(hash_arr) - 1, -1, -1):
         if hash_arr[i] == pattern_hash:
-            print(len(text) - P - i, end=" ")
+            if check_strings(text[len(text) - P - i:len(text) - i], pattern) == 0:
+                print(len(text) - P - i, end=" ")
 
 
 pattern = input()
--- data structures/test.py
+++ data structures/test.py
@@ -1,16 +1,20 @@
-def get_hash(text: str) -> int:
-    m = 5
-    summ = 0
-    p = 1000000007
-    for i in range(0, len(text)):
-        arg = (ord(text[i]) * 263 ** i)
-        summ += arg
+from random import randint
 
-    return summ % p % m
+t0 = 12
+t1 = 423
+p = 146
+x = randint(0, p)
+
+val1 = (t0 * (x ** 1) % p) + (t1 * (x ** 2) % p)
+val2 = ((t0 * (x ** 0) % p) + (t1 * (x ** 1) % p)) * x % p
+val3 = (t0 * (x ** 0) % p) * x % p + (t1 * (x ** 1) % p) * x % p
+print(val1)
+print(val2)
+print(val3)
 
 
-print(get_hash("world"))
-
-
-text = "dshvgavs"
-print(text[0:1])
+print()
+x1 = 124423
+x2 = 7326423
+print((x1 + x2) % p)
+print(x1 % p + x2 % p)
